b2bb32e957456944f897aaba7e7322e067ed2ea3
lying1324a-glitch <lying1324a@gmail.com>
2026-03-08 15:48:51 +0800

    Add ComfyUI KMeans largest-cluster crop node

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from .kmeans_largest_cluster_crop import KMeansLargestClusterCrop
+
+NODE_CLASS_MAPPINGS = {
+    "KMeansLargestClusterCrop": KMeansLargestClusterCrop,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "KMeansLargestClusterCrop": "KMeans Largest Cluster Crop",
+}
