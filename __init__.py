19b7b66c360ce484bbe5ba410a9c696d1b6e6e7d
lying1324a-glitch <lying1324a@gmail.com>
2026-03-08 16:43:54 +0800

    Fix ComfyUI MESH compatibility in mesh-to-pointcloud node

--- __init__.py
+++ __init__.py
@@ -1,9 +1,23 @@
 from .kmeans_largest_cluster_crop import KMeansLargestClusterCrop
+from .pointcloud_nodes import (
+    MeshToPointCloud,
+    PointCloudBoundingBoxSize,
+    PointCloudDBSCANLargestCluster,
+    PointCloudRANSACFilter,
+)
 
 NODE_CLASS_MAPPINGS = {
     "KMeansLargestClusterCrop": KMeansLargestClusterCrop,
+    "MeshToPointCloud": MeshToPointCloud,
+    "PointCloudRANSACFilter": PointCloudRANSACFilter,
+    "PointCloudDBSCANLargestCluster": PointCloudDBSCANLargestCluster,
+    "PointCloudBoundingBoxSize": PointCloudBoundingBoxSize,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "KMeansLargestClusterCrop": "KMeans Largest Cluster Crop",
+    "MeshToPointCloud": "Mesh To Point Cloud",
+    "PointCloudRANSACFilter": "Point Cloud RANSAC Filter",
+    "PointCloudDBSCANLargestCluster": "Point Cloud DBSCAN Largest Cluster",
+    "PointCloudBoundingBoxSize": "Point Cloud Bounding Box Size",
 }
