a0de028cc56325eec4ee7bfb8feeeb16abced0fb
lying1324a-glitch <lying1324a@gmail.com>
2026-03-09 10:28:23 +0800

    Add point-cloud node to remove axis-aligned planes and keep largest surface

--- __init__.py
+++ __init__.py
@@ -1,9 +1,29 @@
 from .kmeans_largest_cluster_crop import KMeansLargestClusterCrop
+from .pointcloud_nodes import (
+    MeshToPointCloud,
+    PointCloudBoundingBoxSize,
+    PointCloudDBSCANLargestCluster,
+    PointCloudRANSACFilter,
+    PointCloudToMesh,
+    PointCloudLargestSurfaceAfterAxisPlaneFilter,
+)
 
 NODE_CLASS_MAPPINGS = {
     "KMeansLargestClusterCrop": KMeansLargestClusterCrop,
+    "MeshToPointCloud": MeshToPointCloud,
+    "PointCloudRANSACFilter": PointCloudRANSACFilter,
+    "PointCloudDBSCANLargestCluster": PointCloudDBSCANLargestCluster,
+    "PointCloudBoundingBoxSize": PointCloudBoundingBoxSize,
+    "PointCloudToMesh": PointCloudToMesh,
+    "PointCloudLargestSurfaceAfterAxisPlaneFilter": PointCloudLargestSurfaceAfterAxisPlaneFilter,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "KMeansLargestClusterCrop": "KMeans Largest Cluster Crop",
+    "MeshToPointCloud": "Mesh To Point Cloud",
+    "PointCloudRANSACFilter": "Point Cloud RANSAC Filter",
+    "PointCloudDBSCANLargestCluster": "Point Cloud DBSCAN Largest Cluster",
+    "PointCloudBoundingBoxSize": "Point Cloud Bounding Box Size",
+    "PointCloudToMesh": "Point Cloud To Mesh",
+    "PointCloudLargestSurfaceAfterAxisPlaneFilter": "Point Cloud Largest Surface After Axis Plane Filter",
 }
